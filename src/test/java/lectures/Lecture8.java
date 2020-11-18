80923f5795f7cb2fe855a81fbb65e7c0240c5600
joansgarcia <joan_sneider@hotmail.com>
2020-11-18 18:39:32 -0500

    Lecture 8

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -5,6 +5,7 @@ import beans.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -16,7 +17,14 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> grouping = MockData.getCars()
+        .stream()
+        .collect(Collectors.groupingBy(Car::getMake));
 
+    grouping.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
@@ -34,6 +42,10 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> counting = names.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    counting.forEach((name, count) -> System.out.println(name + " > " + count));
 
   }
 
