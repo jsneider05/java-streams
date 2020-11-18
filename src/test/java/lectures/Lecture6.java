3025c26bcc9be5af62182bd3ab716d57e6e636bf
joansgarcia <joan_sneider@hotmail.com>
2020-11-18 13:34:33 -0500

    Lecture 6

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,7 +1,6 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
 import java.util.function.Predicate;
 import org.junit.Test;
@@ -12,13 +11,29 @@ public class Lecture6 {
 
   @Test
   public void findAny() throws Exception {
+    //Nondeterministic
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int any = Arrays.stream(numbers)
+        .filter(numbersLessThan10)
+        .findAny()
+        .orElse(0);
+
+    System.out.println(any);
 
   }
 
   @Test
   public void findFirst() throws Exception {
+    // Deterministic
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int first = Arrays.stream(numbers)
+        .filter(numbersLessThan10)
+        .findFirst()
+        .orElse(0);
+
+    System.out.println(first);
 
   }
 }
