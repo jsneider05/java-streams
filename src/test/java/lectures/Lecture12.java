23476a8371c6ebf080cf1d6c18847c9973e83e7b
joansgarcia <joan_sneider@hotmail.com>
2020-11-19 12:58:56 -0500

    Lecture 12

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -3,13 +3,30 @@ package lectures;
 import beans.Person;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture12 {
+
   @Test
   public void understandingCollect() throws Exception {
+//    List<String> emails = MockData.getPeople()
+//        .stream()
+//        .map(Person::getEmail)
+//        .collect(() -> new ArrayList<String>(),
+//            (list, element) -> list.add(element),
+//            (list1, list2) -> list1.addAll(list2));
+
+//    List<String> emails = MockData.getPeople()
+//        .stream()
+//        .map(Person::getEmail)
+//        .collect(ArrayList::new,
+//            List::add,
+//            List::addAll);
+
     List<String> emails = MockData.getPeople()
         .stream()
         .map(Person::getEmail)
