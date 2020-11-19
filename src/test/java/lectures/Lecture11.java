1b91b2581c47c70500467e1c15d818889f94b74a
joansgarcia <joan_sneider@hotmail.com>
2020-11-18 21:15:00 -0500

    Lecture 11

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,40 @@
 package lectures;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.stream.Collectors;
-import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
+  
+  private static final List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+  private static final String STRING_EXPECTED = "[ANNA|JOHN|MARCOS|HELENA|YASMIN]";
 
   @Test
   public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    StringBuilder join = new StringBuilder();
+
+    for (String name : names) {
+      join.append(name.toUpperCase()).append("|");
+    }
+
+//    join = new StringBuilder("[" + join.substring(0, join.length() - 1) + "]");
+    join = new StringBuilder().append("[").append(join.substring(0, join.length() - 1)).append("]");
+    assertThat(join.toString()).hasToString(STRING_EXPECTED);
+    System.out.println(join);
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    String join = names.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.joining("|", "[", "]"));
+
+    assertThat(join).hasToString(STRING_EXPECTED);
+    System.out.println(join);
   }
 }
