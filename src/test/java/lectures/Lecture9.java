776110a1c143883b3e80da03707757996a23bc97
joansgarcia <joan_sneider@hotmail.com>
2020-11-18 20:08:41 -0500

    Lecture 9

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -11,6 +11,13 @@ public class Lecture9 {
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
 
+//    int sum = Arrays.stream(integers)
+//        .reduce(0, (a, b) -> a + b);
+
+    int sum = Arrays.stream(integers)
+        .reduce(0, Integer::sum);
+
+    System.out.println(sum);
   }
 
 
