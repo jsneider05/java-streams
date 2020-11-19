e203eb68a361d1c370ce32684316d2b9c733f908
joansgarcia <joan_sneider@hotmail.com>
2020-11-18 20:40:07 -0500

    Lecture 10

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -1,6 +1,8 @@
 package lectures;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,17 @@ public class Lecture10 {
       Lists.newArrayList("Susy", "Ali")
   );
 
+  private static final ArrayList<String> namesExpected = Lists.newArrayList(
+      "Mariam",
+      "Alex",
+      "Ismail",
+      "John",
+      "Alesha",
+      "Andre",
+      "Susy",
+      "Ali"
+  );
+
   @Before
   public void setUp() {
     System.out.println(arrayListOfNames);
@@ -25,12 +38,46 @@ public class Lecture10 {
   public void withoutFlatMap() throws Exception {
 //    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
 
+    List<String> names = new ArrayList<>();
+
+//    for (List<String> listOfNames : arrayListOfNames) {
+//      for (String name : listOfNames) {
+//        names.add(name);
+//      }
+//    }
+
+//    for (List<String> listOfNames : arrayListOfNames) {
+//      names.addAll(listOfNames);
+//    }
+
+//    arrayListOfNames.forEach(list -> {
+//      list.forEach(name -> listOfNames.add(name));
+//    });
+
+//    arrayListOfNames.forEach(list -> {
+//      list.forEach(listOfNames::add);
+//    });
+
+//    arrayListOfNames.forEach(list -> {
+//      listOfNames.addAll(list);
+//    });
+
+    arrayListOfNames.forEach(names::addAll);
+
+    assertThat(names).isEqualTo(namesExpected);
+    System.out.println(names);
   }
 
   @Test
   public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
 
+    List<String> names = arrayListOfNames.stream()
+        .flatMap(List::stream)
+        .collect(Collectors.toList());
+
+    assertThat(names).isEqualTo(namesExpected);
+    System.out.println(names);
   }
 
 }
